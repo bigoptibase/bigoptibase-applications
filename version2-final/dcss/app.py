6480539d3290999fde618af069d53e330efed51f
Dimitris Pantazatos <dim.pantazatos@gmail.com>
2023-12-18 16:58:27 +0200

    Update app.py

--- version2-final/dcss/app.py
+++ version2-final/dcss/app.py
@@ -28,11 +28,11 @@ ifxh = InfluxHelper()
 
 # CHANNEL_DICT as provided
 CHANNEL_DICT = {
-    '01': 'power_1',
-    '02': 'power_2',
-    '03': 'power_3',
-    '04': 'power_4',
-    '05': 'temperature_1',
+    '01': 'wattage',
+    '02': 'wattage_2',
+    '03': 'wattage_3',
+    '04': 'wattage_4',
+    '05': 'temperature',
     '13': 'temperature_2',
     '21': 'temperature_3',
     'Rh': 'humidity'
@@ -68,11 +68,11 @@ def setup_periodic_tasks(sender, **kwargs):
 
     # Upload Channel 03 - Power C
     sender.add_periodic_task(float(INTERVAL_DICT['03']), upload_channel_data.s('03', int(INTERVAL_DICT['03'])),
-                             name='Uploading {}'.format(CHANNEL_DICT['03']))
+                            name='Uploading {}'.format(CHANNEL_DICT['03']))
 
     # Upload Channel 04 - Power D
     sender.add_periodic_task(float(INTERVAL_DICT['04']), upload_channel_data.s('04', int(INTERVAL_DICT['04'])),
-                             name='Uploading {}'.format(CHANNEL_DICT['04']))
+                            name='Uploading {}'.format(CHANNEL_DICT['04']))
 
     # Upload Channel Rh - Humidity
     sender.add_periodic_task(float(INTERVAL_DICT['Rh']), upload_channel_data.s('Rh', int(INTERVAL_DICT['Rh'])),
@@ -113,7 +113,7 @@ def collect_power():
         power = ifxh.get_latest_value(channel)
 
         if power is not None:
-            logger.debug(f'Power value from channel {channel} ({channel_key}) retrieved: {power}')
+            logger.debug(f'Wattage value from channel {channel} ({channel_key}) retrieved: {power}')
             # Store the value in Redis under the channel-specific key
             red.lpush(channel_key, power)
         else:
